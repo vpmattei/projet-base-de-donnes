0514bc2ff405a38073ea69b2135b9ebd706c3e68
l3miage-matteiv <77281307+l3miage-matteiv@users.noreply.github.com>
2021-03-27 23:24:25 +0100

    Final version

--- src/GUIClient.java
+++ src/GUIClient.java
@@ -1,19 +1,25 @@
+import java.io.File;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class GUIClient extends JFrame {
     private GUILogin guiLogin;
-    private JDBC jdbc;
     private Client loggedClient;
 
     private JPanel backgroundPanel;
     private JPanel bluePanel;
-    private JLabel chooseImageLabel;
     private JLabel clientNameLabel;
     private JFileChooser jFileChooser1;
     private JButton logoutButton;
-    private JPanel imagePanel;
-    private JScrollBar vScrollBar;
-    private JScrollBar hScrollBar;
+    private JButton browseButton;
+    
+    // Image
+    JFrame imageFrame;
+    JLabel imageLabel;
+    BufferedImage img;
+    ImageIcon icon;
     
      public GUIClient(Client client) {
         this.loggedClient = client;
@@ -23,12 +29,11 @@ public class GUIClient extends JFrame {
         backgroundPanel = new JPanel();
         bluePanel = new JPanel();
         clientNameLabel = new JLabel();
-        chooseImageLabel = new JLabel();
-        jFileChooser1 = new JFileChooser();
+        
         logoutButton = new JButton();
-        imagePanel = new JPanel();
-        vScrollBar = new JScrollBar();
-        hScrollBar = new JScrollBar();
+        browseButton = new JButton();
+
+        imageLabel = new JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -57,8 +62,38 @@ public class GUIClient extends JFrame {
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        chooseImageLabel.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        chooseImageLabel.setText("Choose an image :");
+        browseButton.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        browseButton.setText("Browse");
+        browseButton.addActionListener(e ->
+        {
+            jFileChooser1 = new JFileChooser();
+            jFileChooser1.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            int res = jFileChooser1.showOpenDialog(null);
+            // We have an image!
+            try {
+                if (res == JFileChooser.APPROVE_OPTION) {
+                    // Choose and read imgFile
+                    File file = jFileChooser1.getSelectedFile();
+                    img = ImageIO.read(file);
+                    // Create new frame and label for the image
+                    icon = new ImageIcon(img);
+                    imageFrame = new JFrame();
+                    imageFrame.setSize(1500,1800);
+                    imageLabel = new JLabel(icon);
+                    // Show image
+                    imageFrame.add(imageLabel);
+                    imageFrame.setVisible(true);
+                    imageFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                } // Oops!
+                else {
+                    JOptionPane.showMessageDialog(null,
+                            "You must select one image to be the reference.", "Aborting...",
+                            JOptionPane.WARNING_MESSAGE);
+                }
+            } catch (Exception iOException) {
+                iOException.printStackTrace();
+            }
+        });
 
         logoutButton.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         logoutButton.setText("Logout");
@@ -69,79 +104,47 @@ public class GUIClient extends JFrame {
             this.dispose();
         });
 
-        hScrollBar.setOrientation(javax.swing.JScrollBar.HORIZONTAL);
-
-        javax.swing.GroupLayout imagePanelLayout = new javax.swing.GroupLayout(imagePanel);
-        imagePanel.setLayout(imagePanelLayout);
-        imagePanelLayout.setHorizontalGroup(
-            imagePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, imagePanelLayout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(vScrollBar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(imagePanelLayout.createSequentialGroup()
-                .addComponent(hScrollBar, javax.swing.GroupLayout.PREFERRED_SIZE, 552, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
-        );
-        imagePanelLayout.setVerticalGroup(
-            imagePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, imagePanelLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(vScrollBar, javax.swing.GroupLayout.PREFERRED_SIZE, 551, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(hScrollBar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-        );
-
         javax.swing.GroupLayout backgroundPanelLayout = new javax.swing.GroupLayout(backgroundPanel);
         backgroundPanel.setLayout(backgroundPanelLayout);
         backgroundPanelLayout.setHorizontalGroup(
             backgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(bluePanel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 1296, Short.MAX_VALUE)
+            .addComponent(bluePanel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 630, Short.MAX_VALUE)
             .addGroup(backgroundPanelLayout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(backgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(backgroundPanelLayout.createSequentialGroup()
-                        .addComponent(chooseImageLabel)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addComponent(browseButton)
+                        .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(backgroundPanelLayout.createSequentialGroup()
-                        .addGroup(backgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jFileChooser1, javax.swing.GroupLayout.PREFERRED_SIZE, 447, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(logoutButton))
-                        .addGap(18, 18, 18)
-                        .addComponent(imagePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGap(269, 269, 269))))
+                        .addGap(0, 523, Short.MAX_VALUE)
+                        .addComponent(logoutButton)
+                        .addContainerGap())))
         );
         backgroundPanelLayout.setVerticalGroup(
             backgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(backgroundPanelLayout.createSequentialGroup()
                 .addComponent(bluePanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(35, 35, 35)
-                .addComponent(chooseImageLabel)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(backgroundPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(backgroundPanelLayout.createSequentialGroup()
-                        .addComponent(jFileChooser1, javax.swing.GroupLayout.PREFERRED_SIZE, 446, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(logoutButton)
-                        .addGap(67, 67, 67))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, backgroundPanelLayout.createSequentialGroup()
-                        .addComponent(imagePanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addContainerGap())))
+                .addGap(34, 34, 34)
+                .addComponent(browseButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 95, Short.MAX_VALUE)
+                .addComponent(logoutButton)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(backgroundPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 1047, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(backgroundPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(backgroundPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(backgroundPanel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
         setVisible(true);
     }
+
+    
 }
